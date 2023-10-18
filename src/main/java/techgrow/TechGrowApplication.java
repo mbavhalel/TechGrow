bae798e7d684fea6fca62f43d2d79837469b5b06
Mbavhalelo <mbavhalelosurprise@gmail.com>
2023-10-18 20:37:37 +0200

    1st just connected database

---
+++ src/main/java/techgrow/TechGrowApplication.java
@@ -0,0 +1,13 @@
+package techgrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechGrowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechGrowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/techgrow/TechGrowApplicationTests.java
@@ -0,0 +1,13 @@
+package techgrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechGrowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
